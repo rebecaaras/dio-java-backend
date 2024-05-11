7572f064be0020cd6930e5233aced61930f35598
rebecaaras <rebecaaraujo2013@gmail.com>
2024-05-11 15:35:41 -0300

    Atualiza arquivos tipos e variaveis e deleta arquivo de anatomia de classes

---
+++ tipos-e-variaveis/src/TiposEVariaveis.java
@@ -0,0 +1,10 @@
+public class TiposEVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = numeroNormal;
+
+        final double PI = 3.1415;
+        
+    }
+}
