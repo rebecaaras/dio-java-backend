5a4bca8345e53df05e29e816ff7dbf3656e71616
rebecaaras <rebecaaraujo2013@gmail.com>
2024-05-18 22:03:39 -0300

    Adiciona exercícios sobre estruturas de repetição FOR e WHILE

---
+++ controle-fluxo/exemplos/src/ExemploAlunos.java
@@ -0,0 +1,11 @@
+public class ExemploAlunos {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no índice i = " + i + " é " + alunos[i]);
+            
+        }
+
+    }
+}
---
+++ controle-fluxo/exemplos/src/ExemploForEach.java
@@ -0,0 +1,10 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        for (String aluno:alunos) {
+            System.out.println(aluno);
+        }
+}
+    }
+    
